158e76670c70e8eb6ea14f8bb8fb948750dd8231
ranjaykumar73 <ranjaykumar73.rk@gmail.com>
2023-12-28 17:06:38 +0530

    changes in projectDATA

--- src/data/ProjectData.js
+++ src/data/ProjectData.js
@@ -1,18 +1,18 @@
 export const projectData = [
     {
-      title:'Project 1',
+      title:'Hotstar-clone',
       description:'Description of Project1',
       projectLink:'https://github.com/ranjaykumar73/hotstar-clone',
       projectURL:'https://lambent-cactus-80c64c.netlify.app/'
     },
     {
-      title:'Project 2',
+      title:'weather-broadcast',
       description:'Description of Project 2',
       projectLink:'https://github.com/ranjaykumar73/weatherApp',
       projectURL:'https://weatherforcastrk.netlify.app/'
     },
     {
-      title:'Project 3',
+      title:'Event-Management',
       description:'Description of Project 3',
       projectLink:'https://github.com/ranjaykumar73/my-event',
       projectURL:'https://ranjay-event-management-ea5c6f.netlify.app/'
